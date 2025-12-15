7f66b3d777b26a49158ad192f7d8fdc780504a9e
unknown <rifdahnsyh@gmail.com>
2025-12-15 20:26:39 +0700

    Day 1: setup laravel, database, relasi

---
+++ app/Models/Pendaftar.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Pendaftar extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'nama',
+        'email',
+        'sekolah_asal',
+        'prodi_id',
+        'dokumen',
+        'status'
+    ];
+
+    public function prodi()
+    {
+        return $this->belongsTo(Prodi::class);
+    }
+}
